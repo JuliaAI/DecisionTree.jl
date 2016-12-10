3c4dced40691c0fd187c1f851ea010d9916a6d1a
Ben Sadeghi <ben.sadeghi1@gmail.com>
2016-12-10 13:57:47 +0800

    Resolved #41

--- test/classification_rand.jl
+++ test/classification_rand.jl
@@ -1,6 +1,8 @@
 using Base.Test
 using DecisionTree
 
+srand(16)
+
 n,m = 10^3, 5 ;
 features = rand(n,m);
 weights = rand(-1:1,m);
@@ -19,4 +21,5 @@ accuracy = nfoldCV_forest(labels, features, 2, 10, 3)
 @test mean(accuracy) > 0.7
 
 println("\n##### nfoldCV Adaboosted Stumps #####")
-nfoldCV_stumps(labels, features, 7, 3)
+accuracy = nfoldCV_stumps(labels, features, 7, 3)
+@test mean(accuracy) > 0.5
