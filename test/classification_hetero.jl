b15d985287fe0fbca9198939a67c37b13d996056
Ben Sadeghi <ben.sadeghi1@gmail.com>
2017-11-01 08:01:45 +0000

    update hetero test

--- test/classification_hetero.jl
+++ test/classification_hetero.jl
@@ -1,5 +1,6 @@
 ### Classification - Heterogeneously typed features (ints, floats, bools, strings)
 
+using Base.Test
 using DecisionTree
 
 m, n = 10^2, 5;
@@ -11,10 +12,21 @@ labels = string.(tf[inds]);
 features = Array{Any}(m, n);
 features[:,:] = randn(m, n);
 features[:,2] = string.(tf[randperm(m)]);
-features[:,3] = round.(Int, features[:,3]);
+features[:,3] = round(Int, features[:,3]);
 features[:,4] = tf[inds];
 
-build_tree(labels, features)
-build_forest(labels, features,2,3)
-build_stump(labels, features)
+model = build_tree(labels, features);
+preds = apply_tree(model, features);
+cm = confusion_matrix(labels, preds);
+@test cm.accuracy > 0.7
+
+model = build_forest(labels, features,2,3);
+preds = apply_forest(model, features);
+cm = confusion_matrix(labels, preds);
+@test cm.accuracy > 0.7
+
+model, coeffs = build_adaboost_stumps(labels, features, 7);
+preds = apply_adaboost_stumps(model, coeffs, features);
+cm = confusion_matrix(labels, preds);
+@test cm.accuracy > 0.7
 
