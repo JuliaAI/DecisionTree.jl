43a0eadfb54809bee3e2a636879e346f41e6a402
Ben Sadeghi <ben.sadeghi1@gmail.com>
2017-11-02 01:38:51 +0000

    added test using adult dataset

---
+++ test/adult.jl
@@ -0,0 +1,17 @@
+using Base.Test
+using DecisionTree
+
+run(`wget https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data`);
+adult = readcsv("adult.data");
+
+features = adult[:, 2:14];
+labels = adult[:, 15];
+
+println("\n##### 3 foldCV Classification Tree #####")
+accuracy = nfoldCV_tree(labels, features, 0.9, 3);
+@test mean(accuracy) > 0.8
+
+println("\n##### 3 foldCV Classification Forest #####")
+accuracy = nfoldCV_forest(labels, features, 2, 10, 3, 0.5);
+@test mean(accuracy) > 0.8
+
