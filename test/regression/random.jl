9c1a6b3ffa29104d851299da6c0422aef1ec35f8
Ben Sadeghi <ben.sadeghi1@gmail.com>
2018-06-15 14:27:30 +0800

    minor updates to regression tests

--- test/regression/random.jl
+++ test/regression/random.jl
@@ -1,14 +1,21 @@
-using Base.Test
-using DecisionTree
-
 srand(5)
+
 n,m = 10^3, 5 ;
 features = randn(n,m);
 weights = rand(-2:2,m);
 labels = features * weights;
-maxdepth = 3
-model = build_tree(labels, features, 5, 0, maxdepth)
-@test depth(model) == maxdepth
+
+# over-fitting
+max_features = 1
+model = build_tree(labels, features, max_features)
+preds = apply_tree(model, features)
+@test R2(labels, preds) > 0.99      # R2: coeff of determination
+
+max_features = 5
+max_depth = 3
+nsubfeatures = 0                    # all features
+model = build_tree(labels, features, max_features, nsubfeatures, max_depth)
+@test depth(model) == max_depth
 
 println("\n##### nfoldCV Regression Tree #####")
 r2 = nfoldCV_tree(labels, features, 3)
