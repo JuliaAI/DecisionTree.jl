b59d74aa17a089389f020b267e8ac5e6b0696455
Ben Sadeghi <ben.sadeghi1@gmail.com>
2018-06-16 11:28:44 +0800

    added energy.jl regression test

---
+++ test/regression/energy.jl
@@ -0,0 +1,22 @@
+# Regression Test - Appliances Energy Prediction Data Set
+# https://archive.ics.uci.edu/ml/datasets/Appliances+energy+prediction
+
+download("https://archive.ics.uci.edu/ml/machine-learning-databases/00374/energydata_complete.csv", "energy.csv");
+energy = readcsv("energy.csv");
+
+features = float.(energy[2:end, 3:end]);
+labels = float.(energy[2:end, 2]);
+
+# over-fitting
+min_samples_leaf = 1;
+model = build_tree(labels, features, min_samples_leaf)
+preds = apply_tree(model, features);
+@test R2(labels, preds) > 0.99
+
+println("\n##### nfoldCV Regression Tree #####")
+r2 = nfoldCV_tree(labels, features, 3);
+@test mean(r2) > 0.05
+
+println("\n##### nfoldCV Regression Forest #####")
+r2 = nfoldCV_forest(labels, features, 2, 10, 3);
+@test mean(r2) > 0.4
