0333bd231a98d09af45fb89eddc9e959eb1c532b
Ben Sadeghi <ben.sadeghi1@gmail.com>
2018-08-11 20:07:43 +0800

    update load_data

--- src/load_data.jl
+++ src/load_data.jl
@@ -1,4 +1,5 @@
 function load_data(name)
+    datasets = ["iris", "adult", "digits"]
     data_path = joinpath(dirname(pathof(DecisionTree)), "..", "test/data/")
 
     if name == "digits"
@@ -10,20 +11,24 @@ function load_data(name)
         data = hcat(data...)
         Y = Int.(data[1, 1:end]) .+ 1
         X = convert(Matrix, transpose(data[2:end, 1:end]))
-        return X, Y    
+        return X, Y
     end
 
     if name == "iris"
         iris = DelimitedFiles.readdlm(joinpath(data_path, "iris.csv"), ',')
         X = iris[:, 1:4]
         Y = iris[:, 5]
-        return X, Y    
+        return X, Y
     end
 
     if name == "adult"
         adult = DelimitedFiles.readdlm(joinpath(data_path, "adult.csv"), ',');
         X = adult[:, 1:14];
         Y = adult[:, 15];
-        return X, Y    
+        return X, Y
+    end
+
+    if !(name in datasets)
+        throw("Available datasets are $(join(datasets,", "))")
     end
 end
