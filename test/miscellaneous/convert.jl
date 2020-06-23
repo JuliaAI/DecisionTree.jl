fcad48c0ffaf9b8fb882a4e23d16c2191fcf1c39
Ben Sadeghi <ben.sadeghi1@gmail.com>
2020-06-23 09:06:33 +0800

    add convert tests

--- test/miscellaneous/convert.jl
+++ test/miscellaneous/convert.jl
@@ -3,10 +3,18 @@
 @testset "convert.jl" begin
 
 lf = Leaf(1, [1])
-
 nv = Node{Int, Int}[]
 push!(nv, lf)
-
 @test apply_tree(nv[1], [0]) == 1
+
+lf = Leaf(1.0, [0.0, 1.0])
+nv = Node{Int, Float64}[]
+push!(nv, lf)
+@test apply_tree(nv[1], [0]) == 1.0
+
+lf = Leaf("A", ["B", "A"])
+nv = Node{Int, String}[]
+push!(nv, lf)
+@test apply_tree(nv[1], [0]) == "A"
 
 end # @testset
