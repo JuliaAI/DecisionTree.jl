c0cd4e802b2f78b200d5b6fa4663d0039ebc844e
giopaglia <24519853+giopaglia@users.noreply.github.com>
2021-03-27 10:36:10 +0100

    Fix local.jl

--- src/local.jl
+++ src/local.jl
@@ -1,2 +1,3 @@
-data_dir = "/home/gio/Desktop/SpatialDecisionTree/DecisionTree.jl/"
+data_dir = "../"
+# data_dir = "/home/gio/Desktop/SpatialDecisionTree/DecisionTree.jl/"
 # data_dir = "/home/gpagliarini/ModalDecisionTrees/"
