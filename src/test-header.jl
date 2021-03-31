704f252349955e1fa9d5e326e2ed257bb6f10a4b
ferdiu <ferdiu.manzella@gmail.com>
2021-03-31 17:16:12 +0200

    'testDatabase' now returns generated tree (and/or forest) and its confusion matrix as a Tuple

--- src/test-header.jl
+++ src/test-header.jl
@@ -46,7 +46,31 @@ function testDataset(name::String, dataset::Tuple, split_threshold::Union{Bool,A
 	# global_logger(ConsoleLogger(stderr, Logging.Info))
 	global_logger(ConsoleLogger(stderr, debugging_level))
 	# global_logger(ConsoleLogger(stderr, DecisionTree.DTDebug))
-	
+
+	function display_cm_as_row(cm::ConfusionMatrix)
+		"|\t" *
+		"$(round(cm.overall_accuracy*100, digits=2))%\t" *
+		"$(round(cm.mean_accuracy*100, digits=2))%\t" *
+		"$(round(cm.kappa*100, digits=2))%\t" *
+		# "$(round(DecisionTree.macro_F1(cm)*100, digits=2))%\t" *
+		"$(round.(cm.accuracies.*100, digits=2))%\t" *
+		"$(round.(cm.F1s.*100, digits=2))%\t" *
+		"$(round.(cm.sensitivities.*100, digits=2))%\t" *
+		"$(round.(cm.specificities.*100, digits=2))%\t" *
+		"$(round.(cm.PPVs.*100, digits=2))%\t" *
+		"$(round.(cm.NPVs.*100, digits=2))%\t" *
+		"||\t" *
+		"$(round(DecisionTree.macro_weighted_F1(cm)*100, digits=2))%\t" *
+		# "$(round(DecisionTree.macro_sensitivity(cm)*100, digits=2))%\t" *
+		"$(round(DecisionTree.macro_weighted_sensitivity(cm)*100, digits=2))%\t" *
+		# "$(round(DecisionTree.macro_specificity(cm)*100, digits=2))%\t" *
+		"$(round(DecisionTree.macro_weighted_specificity(cm)*100, digits=2))%\t" *
+		# "$(round(DecisionTree.mean_PPV(cm)*100, digits=2))%\t" *
+		"$(round(DecisionTree.macro_weighted_PPV(cm)*100, digits=2))%\t" *
+		# "$(round(DecisionTree.mean_NPV(cm)*100, digits=2))%\t" *
+		"$(round(DecisionTree.macro_weighted_NPV(cm)*100, digits=2))%\t"
+	end
+
 	go_tree() = begin
 		if timeit == 0
 			T = build_tree(Y_train, X_train; args..., kwargs..., rng = rng);
@@ -58,6 +82,7 @@ function testDataset(name::String, dataset::Tuple, split_threshold::Union{Bool,A
 		print(T)
 		
 		println(" test size = $(size(X_test))")
+		cm = nothing
 		for pruning_purity_threshold in sort(unique([(Float64.(post_pruning_purity_thresholds))...,1.0]))
 			println(" Purity threshold $pruning_purity_threshold")
 			
@@ -67,30 +92,6 @@ function testDataset(name::String, dataset::Tuple, split_threshold::Union{Bool,A
 			preds = apply_tree(T_pruned, X_test);
 			cm = confusion_matrix(Y_test, preds)
 			# @test cm.overall_accuracy > 0.99
-			
-			function display_cm_as_row(cm::ConfusionMatrix)
-				"|\t" *
-				"$(round(cm.overall_accuracy*100, digits=2))%\t" *
-				"$(round(cm.mean_accuracy*100, digits=2))%\t" *
-				"$(round(cm.kappa*100, digits=2))%\t" *
-				# "$(round(DecisionTree.macro_F1(cm)*100, digits=2))%\t" *
-				"$(round.(cm.accuracies.*100, digits=2))%\t" *
-				"$(round.(cm.F1s.*100, digits=2))%\t" *
-				"$(round.(cm.sensitivities.*100, digits=2))%\t" *
-				"$(round.(cm.specificities.*100, digits=2))%\t" *
-				"$(round.(cm.PPVs.*100, digits=2))%\t" *
-				"$(round.(cm.NPVs.*100, digits=2))%\t" *
-				"||\t" *
-				"$(round(DecisionTree.macro_weighted_F1(cm)*100, digits=2))%\t" *
-				# "$(round(DecisionTree.macro_sensitivity(cm)*100, digits=2))%\t" *
-				"$(round(DecisionTree.macro_weighted_sensitivity(cm)*100, digits=2))%\t" *
-				# "$(round(DecisionTree.macro_specificity(cm)*100, digits=2))%\t" *
-				"$(round(DecisionTree.macro_weighted_specificity(cm)*100, digits=2))%\t" *
-				# "$(round(DecisionTree.mean_PPV(cm)*100, digits=2))%\t" *
-				"$(round(DecisionTree.macro_weighted_PPV(cm)*100, digits=2))%\t" *
-				# "$(round(DecisionTree.mean_NPV(cm)*100, digits=2))%\t" *
-				"$(round(DecisionTree.macro_weighted_NPV(cm)*100, digits=2))%\t"
-			end
 
 			println("RESULT:\t$(name)\t$(args)\t$(kwargs)\t$(pruning_purity_threshold)\t$(display_cm_as_row(cm))")
 			
@@ -106,7 +107,7 @@ function testDataset(name::String, dataset::Tuple, split_threshold::Union{Bool,A
 
 			# println("nodes: ($(num_nodes(T_pruned)), height: $(height(T_pruned)))")
 		end
-		return T;
+		return (T, cm);
 	end
 
 	go_forest() = begin
@@ -124,30 +125,6 @@ function testDataset(name::String, dataset::Tuple, split_threshold::Union{Bool,A
 		preds = apply_forest(F, X_test);
 		cm = confusion_matrix(Y_test, preds)
 		# @test cm.overall_accuracy > 0.99
-		
-		function display_cm_as_row(cm::ConfusionMatrix)
-			"|\t" *
-			"$(round(cm.overall_accuracy*100, digits=2))%\t" *
-			"$(round(cm.mean_accuracy*100, digits=2))%\t" *
-			"$(round(cm.kappa*100, digits=2))%\t" *
-			# "$(round(DecisionTree.macro_F1(cm)*100, digits=2))%\t" *
-			"$(round.(cm.accuracies.*100, digits=2))%\t" *
-			"$(round.(cm.F1s.*100, digits=2))%\t" *
-			"$(round.(cm.sensitivities.*100, digits=2))%\t" *
-			"$(round.(cm.specificities.*100, digits=2))%\t" *
-			"$(round.(cm.PPVs.*100, digits=2))%\t" *
-			"$(round.(cm.NPVs.*100, digits=2))%\t" *
-			"||\t" *
-			"$(round(DecisionTree.macro_weighted_F1(cm)*100, digits=2))%\t" *
-			# "$(round(DecisionTree.macro_sensitivity(cm)*100, digits=2))%\t" *
-			"$(round(DecisionTree.macro_weighted_sensitivity(cm)*100, digits=2))%\t" *
-			# "$(round(DecisionTree.macro_specificity(cm)*100, digits=2))%\t" *
-			"$(round(DecisionTree.macro_weighted_specificity(cm)*100, digits=2))%\t" *
-			# "$(round(DecisionTree.mean_PPV(cm)*100, digits=2))%\t" *
-			"$(round(DecisionTree.macro_weighted_PPV(cm)*100, digits=2))%\t" *
-			# "$(round(DecisionTree.mean_NPV(cm)*100, digits=2))%\t" *
-			"$(round(DecisionTree.macro_weighted_NPV(cm)*100, digits=2))%\t"
-		end
 
 		println("RESULT:\t$(name)\t$(forest_args)\t$(args)\t$(kwargs)\t$(display_cm_as_row(cm))")
 
@@ -160,16 +137,23 @@ function testDataset(name::String, dataset::Tuple, split_threshold::Union{Bool,A
 		end
 
 		global_logger(ConsoleLogger(stderr, Logging.Info));
-		return F;
+		return (F, cm);
 	end
 
 	go() = begin
+		T = nothing
+		F = nothing
+		Tcm = nothing
+		Fcm = nothing
+
 		if test_tree
-		 	go_tree()
+		 	T, Tcm = go_tree()
 		end
 		if test_forest
-			go_forest()
+			F, Fcm = go_forest()
 		end
+
+		T, F, Tcm, Fcm
 	end
 
 	if error_catching 
