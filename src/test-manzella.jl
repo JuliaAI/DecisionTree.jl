4e7b287593efbffd144229bda1f7ed64c851eb5f
ferdiu <ferdiu.manzella@gmail.com>
2021-04-04 14:41:13 +0200

    test-manzella.jl can now be launched with exactly 5 arguments

--- src/test-manzella.jl
+++ src/test-manzella.jl
@@ -63,6 +63,20 @@ min_purity_increase = 0.01
 # maximum purity allowed on a leaf
 min_loss_at_leaf = 0.4
 
+log_results_best_values = false
+repeat_test = 5
+log_file = "./results-find-best-values/results.csv"
+dataset_number = 5
+
+if length(ARGS) === 5
+	min_samples_leaf = parse(Int64, ARGS[1])
+	min_purity_increase = parse(Float64, ARGS[2])
+	min_loss_at_leaf = parse(Float64, ARGS[3])
+	dataset_number = parse(Int64, ARGS[4])
+	log_file = ARGS[5]
+	log_results_best_values = true
+end
+
 # Best values found for a single tree and forest
 #min_samples_leaf = 1
 #min_purity_increase = 0.01
@@ -91,8 +105,84 @@ n_instances = 100
 # rng_i = DecisionTree.mk_rng(124)
 rng_i = DecisionTree.mk_rng(1)
 
-dataset = SplatEduardDataset(10)
+dataset = SplatEduardDataset(dataset_number)
+
+T = nothing
+Tcm = nothing
+F = nothing
+Fcm = nothing
 
 T, F, Tcm, Fcm = testDataset("Test", dataset, false, 0, debugging_level=log_level,
 	forest_args=forest_args, args=selected_args, kwargs=kwargs,
-	test_tree = false, test_forest = true);
+	test_tree = true, test_forest = true);
+
+function log_to_file(
+		filename,
+		min_samples_leaf,
+		min_purity_increase,
+		min_loss_at_leaf,
+		tree_overall_accuracy,
+		tree_mean_accuracy,
+		tree_kappa,
+		forest_overall_accuracy,
+		forest_mean_accuracy,
+		forest_kappa,
+		forest_oob_error
+	)
+
+    file = open(filename, "a+")
+
+	write(file,
+		string(min_samples_leaf) * "," *
+		string(min_purity_increase) * "," *
+		string(min_loss_at_leaf) * "," *
+		string(round.(mean(tree_overall_accuracy).*100, digits=2)) * "," *
+		string(round.(mean(tree_mean_accuracy).*100, digits=2)) * "," *
+		string(round.(mean(tree_kappa).*100, digits=2)) * "," *
+		string(round.(mean(forest_overall_accuracy).*100, digits=2)) * "," *
+		string(round.(mean(forest_mean_accuracy).*100, digits=2)) * "," *
+		string(round.(mean(forest_kappa).*100, digits=2)) * "," *
+		string(round.(mean(forest_oob_error).*100, digits=2)) * "\n"
+	)
+
+	close(file)
+end
+
+if log_results_best_values
+	tree_overall_accuracy = []
+	tree_mean_accuracy = []
+	tree_kappa = []
+	forest_overall_accuracy = []
+	forest_mean_accuracy = []
+	forest_kappa = []
+	forest_oob_error = []
+
+	for i in 1:repeat_test
+
+		global T, F, Tcm, Fcm = testDataset("Test", dataset, false, 0, debugging_level=log_level,
+			forest_args=forest_args, args=selected_args, kwargs=kwargs,
+			test_tree = true, test_forest = true);
+
+		push!(tree_overall_accuracy, Tcm.overall_accuracy)
+		push!(tree_mean_accuracy, Tcm.mean_accuracy)
+		push!(tree_kappa, Tcm.kappa)
+		push!(forest_overall_accuracy, Fcm.overall_accuracy)
+		push!(forest_mean_accuracy, Fcm.mean_accuracy)
+		push!(forest_kappa, Fcm.kappa)
+		push!(forest_oob_error, F.oob_error)
+	end
+
+	log_to_file(
+		log_file,
+		min_samples_leaf,
+		min_purity_increase,
+		min_loss_at_leaf,
+		tree_overall_accuracy,
+		tree_mean_accuracy,
+		tree_kappa,
+		forest_overall_accuracy,
+		forest_mean_accuracy,
+		forest_kappa,
+		forest_oob_error
+	)
+end
